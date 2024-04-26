69a72813a447099743c61ffc5287bc55b93cdad7
powerworker1234@gmail.com <powerworker1234@gmail.com>
2024-04-26 12:29:18 +0800

    0426 commit

--- app/src/main/java/tw/edu/pu/s1114859/taichung__city_social_welfare_organization_map/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114859/taichung__city_social_welfare_organization_map/MainActivity.kt
@@ -8,6 +8,7 @@ import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import tw.edu.pu.s1114859.taichung__city_social_welfare_organization_map.objects.Record
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var record:Record
         //建立Request，設置連線資訊
         val request: Request = Request.Builder()
-            .url("https://jsonplaceholder.typicode.com/posts/1")
+            .url("https://datacenter.taichung.gov.tw/swagger/OpenData/c2ee604c-e0ec-4776-8f7a-4b840df0caf8")
             .build()
         //建立Call
         val call: Call =client.newCall(request)
@@ -26,11 +28,13 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 //連線成功，自response取得連線結果
                 val result: String = response.body!!.string()
+
                 Log.d("OkHttp result", result)
             }
 
             override fun onFailure(call: Call, e: IOException) {
                 //連線失敗
+                println("error:${e.message}")
             }
         })
     }
---
+++ app/src/main/java/tw/edu/pu/s1114859/taichung__city_social_welfare_organization_map/objects/Record.kt
@@ -0,0 +1,24 @@
+package tw.edu.pu.s1114859.taichung__city_social_welfare_organization_map.objects
+
+import android.provider.DocumentsContract.Root
+import com.google.gson.annotations.SerializedName
+data class Record(
+    @SerializedName("基金會名稱") val name: String,
+    @SerializedName("連絡電話") val phone: String,
+    @SerializedName("傳真") val TEL: String,
+    @SerializedName("電子郵件") val mail: String,
+    @SerializedName("地址") val address: String,
+    @SerializedName("行政區") val ad: String,
+    @SerializedName("X坐標") val X: String,
+    @SerializedName("Y坐標") val Y: String,
+    @SerializedName("相關網址") val link: String,
+    @SerializedName("最後更新日期") val newdate: String,
+    @SerializedName("服務對象") val group: String,
+    @SerializedName("縣市別代碼") val code: String
+)
+data class DataResponse(
+    @SerializedName("ROOT") val root:Root
+)
+data class Root(
+    @SerializedName("RECORD") val records: List<Record>
+)
