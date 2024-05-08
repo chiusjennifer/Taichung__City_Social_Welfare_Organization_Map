30425ba0ba980354b573413552f847d706a6cea4
Jennifer <chiusjennifer@gmail.com>
2024-05-08 16:13:48 +0800

    UI 顯示 opendata

--- app/src/main/java/tw/edu/pu/s1114859/taichung__city_social_welfare_organization_map/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1114859/taichung__city_social_welfare_organization_map/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.edu.pu.s1114859.taichung__city_social_welfare_organization_map
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.google.gson.Gson
 import okhttp3.Call
 import okhttp3.Callback
@@ -13,11 +14,12 @@ import org.json.JSONObject
 import tw.edu.pu.s1114859.taichung__city_social_welfare_organization_map.objects.Record
 import java.io.IOException
 
+
 class MainActivity : AppCompatActivity() {
     private val client: OkHttpClient =OkHttpClient.Builder().build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.recyclerview_item)
         var record:Record
         //建立Request，設置連線資訊
         val request: Request = Request.Builder()
@@ -32,13 +34,21 @@ class MainActivity : AppCompatActivity() {
                 val result: String = response.body!!.string()
                 val rootStr = JSONObject(result).getJSONObject("ROOT").toString()
                 val records = JSONObject(rootStr).getJSONArray("RECORD")
-                for (i in 0 until records.length()){
-                    val recordObject = records.getJSONObject(i)
+                for (i in 0 until 1){
+                    val recordObject = records.getJSONObject(0)
                     val gson = Gson()
                     val data = gson.fromJson(recordObject.toString(),Record::class.java)
-                    Log.d("TAG",data.name)
-                    Log.d("TAG",data.address)
-                 }
+                    // 在 UI 線程上更新 TextView
+                    runOnUiThread {
+                        findViewById<TextView>(R.id.dataname).text = data.name
+                        findViewById<TextView>(R.id.dataphone).text = data.phone
+                        findViewById<TextView>(R.id.dataddress).text = data.address
+                        findViewById<TextView>(R.id.datad).text = data.ad
+                        findViewById<TextView>(R.id.datalink).text = data.link
+                        findViewById<TextView>(R.id.dataob).text = data.group
+                    }
+
+                }
 
 
             }
